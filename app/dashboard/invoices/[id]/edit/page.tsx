3607c886c1fb757a75531efded905e9e730593e7
Foxylabstory <rushan.shakirzianov@gmail.com>
2025-02-26 10:11:44 +0300

    16 chapter

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,6 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
-import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { fetchInvoiceById, fetchCustomers, fetchFilteredCustomers } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
@@ -28,4 +28,22 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
       <Form invoice={invoice} customers={customers} />
     </main>
   );
+}
+
+export async function generateMetadata(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
+  const id = params.id;
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+  let customer = 'Edit'
+  customers.forEach((person) => {
+    if (person.id === invoice.customer_id) {
+      customer = person.name
+    }
+  })
+  return {
+    title: `${customer} | Invoices`,
+  }
 }
